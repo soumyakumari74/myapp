f41d850e5018675f8be304ee70e0f46540d09e02
soumyakumari74 <ksoumyaofficial@gmail.com>
2021-12-19 04:03:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
-
+import data from "./data";
 function App() {
+  const [person1,person2,person3,person4]=data;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <h1>Display of name and id</h1>
+      <br/>
+      <hr></hr>
+      <table>
+        <tr>
+          <th>ID</th>
+          <th>NAME</th>
+        </tr>
+        <tr>
+          <td>{person1.id}</td>
+          <td>{person1.name}</td>
+        </tr>
+        <tr>
+        <td>{person2.id}</td>
+        <td>{person2.name}</td>
+        </tr>
+        <tr>
+        <td>{person3.id}</td>
+        <td>{person3.name}</td>
+        </tr>
+        <tr>
+        <td>{person4.id}</td>
+        <td>{person4.name}</td>
+        </tr>
+      </table>
+      <h3>Github-externship assignment - SOUMYA KUMARI</h3>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,5 @@
+const data=[
+    {id: 1, name: "Item 1" }, { id: 3, name: "Item 3" }, { id: 2, name: "Item 2", }, { id: 4, name: "Item 4" }
+]
+
+export default data;
